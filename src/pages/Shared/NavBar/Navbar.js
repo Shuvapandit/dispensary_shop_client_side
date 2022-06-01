63e7571714f5a0a07e6dcc1e69310fb811682450
Shuvapandit <chakrabortyshuva216@gmail.com>
2022-06-01 22:36:51 +0600

    dispensary_shop_client_side initial commit

---
+++ src/pages/Shared/NavBar/Navbar.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const Navbar = () => {
+ return (
+<div>
+<div className="navbar bg-base-300">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <label tabindex="0" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
+      <ul tabindex="0" className="menu  bg-base-300 menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        <li><a>Home</a></li>
+        <li tabindex="0">
+          <a className="justify-between">
+            About </a>
+
+        </li>
+        <li><a>Blog</a></li>
+        <li><a>Contact</a></li>
+      </ul>
+    </div>
+    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal p-0">
+      <li><a>Home</a></li>
+      <li tabindex="0">
+        <a>
+         About
+        </a>
+        </li>
+      <li><a>Blog</a></li>
+      <li><a>Contact</a></li>
+    </ul>
+  </div>
+  <div className='navbar-end mr-3'>
+ 
+<button className="btn btn-ghost btn-circle">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+    </button>
+  <div className="indicator">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+          <span className="badge badge-sm indicator-item">0</span>
+      </div>
+  </div>
+</div>
+
+                                                   
+</div>
+);
+};
+
+export default Navbar;
